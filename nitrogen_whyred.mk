cf2f9d3c04afae56852b0120f9d2829adae3f65a
rahul1892 <kumar.rahul1892.gmail.com>
2018-10-31 00:00:22 +0530

    Whyred: Nitrogen OS Bringup
    I hope, i didn't mak some silly spelling mistakes.

--- nitrogen_whyred.mk
+++ nitrogen_whyred.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit from whyred device
@@ -25,16 +25,16 @@ $(call inherit-product, device/xiaomi/whyred/device.mk)
 # Inherit from custom vendor
 $(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
 
-# Inherit some common PixelExperience stuff.
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common NitrogenOS Stuffs
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := nitrogen_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Redmi Note 5
+PRODUCT_MODEL := Redmi Note 5 Pro
+
+TARGET_BOOT_ANIMATION := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
